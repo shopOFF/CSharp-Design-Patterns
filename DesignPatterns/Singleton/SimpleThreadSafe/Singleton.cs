17de0039ff2c9411bade4b28315f2fc957e7e698
shopov91 <shopov91@gmail.com>
2017-02-20 23:42:14 +0200

    Added simple thread safe singleton

---
+++ DesignPatterns/Singleton/SimpleThreadSafe/Singleton.cs
@@ -0,0 +1,28 @@
+﻿namespace Singleton.SimpleThreadSafe
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+        private static readonly object padlock = new object();
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+
+                    return instance;
+                }
+            }
+        }
+    }
+}
