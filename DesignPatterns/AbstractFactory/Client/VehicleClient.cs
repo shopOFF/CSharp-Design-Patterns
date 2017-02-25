c305c55e633a7dcb3e13ecd89ebd525f539cf01d
shopov91 <shopov91@gmail.com>
2017-02-25 21:45:19 +0200

    Implemented completely

---
+++ DesignPatterns/AbstractFactory/Client/VehicleClient.cs
@@ -0,0 +1,35 @@
+﻿using AbstractFactory.Contracts;
+
+namespace AbstractFactory.Client
+{
+    /// <summary>
+    /// The 'Client' class 
+    /// </summary>
+    public class VehicleClient
+    {
+        private IBike bike;
+        private IScooter scooter;
+
+        public VehicleClient(IVehicleFactory factory, string type)
+        {
+            this.bike = factory.GetBike(type);
+            this.scooter = factory.GetScooter(type);
+        }
+
+        public string GetBikeName
+        {
+            get
+            {
+                return this.bike.Name();
+            }
+        }
+
+        public string GetScooterName
+        {
+            get
+            {
+                return this.scooter.Name();
+            }
+        }
+    }
+}
--- DesignPatterns/AbstractFactory/StartUpDemo.cs
+++ DesignPatterns/AbstractFactory/StartUpDemo.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AbstractFactory.Client;
+using AbstractFactory.Contracts;
+using AbstractFactory.Factories;
+using System;
 
 namespace AbstractFactory
 {
+    /// <summary>
+    /// Abstract Factory Pattern Demo
+    /// </summary>
     public class StartUpDemo
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // Honda Factory
+            IVehicleFactory hondaFactory = new HondaFactory();
+            VehicleClient hondaClient = new VehicleClient(hondaFactory, "Regular");
+
+            Console.WriteLine("******* Honda **********");
+            Console.WriteLine($"{hondaClient.GetBikeName}");
+            Console.WriteLine($"{hondaClient.GetScooterName}");
+
+            hondaClient = new VehicleClient(hondaFactory, "Sports");
+            Console.WriteLine($"{hondaClient.GetBikeName}");
+            Console.WriteLine($"{hondaClient.GetScooterName}");
+
+            // Hero Factory
+            IVehicleFactory heroFactory = new HondaFactory();
+            VehicleClient heroClient = new VehicleClient(heroFactory, "Regular");
+
+            Console.WriteLine("******* Hero **********");
+            Console.WriteLine($"{heroClient.GetBikeName}");
+            Console.WriteLine($"{heroClient.GetScooterName}");
+
+            heroClient = new VehicleClient(heroFactory, "Sports");
+            Console.WriteLine($"{heroClient.GetBikeName}");
+            Console.WriteLine($"{heroClient.GetScooterName}");
         }
     }
 }
