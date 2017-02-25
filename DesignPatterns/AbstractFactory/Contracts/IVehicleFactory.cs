cd9d711ce804e452f83bcc77bf46cd858b76c04e
shopov91 <shopov91@gmail.com>
2017-02-25 21:07:22 +0200

    Implemented IVehicleFactory and models

---
+++ DesignPatterns/AbstractFactory/Contracts/IVehicleFactory.cs
@@ -0,0 +1,14 @@
+﻿using AbstractFactory.Models;
+
+namespace AbstractFactory.Contracts
+{
+    /// <summary>
+    /// The 'AbstractFactory' interface. 
+    /// </summary>
+    public interface IVehicleFactory
+    {
+        Bike GetBike(string bike);
+
+        Scooter GetScooter(string scooter);
+    }
+}
---
+++ DesignPatterns/AbstractFactory/Factories/HondaFactory.cs
@@ -0,0 +1,19 @@
+﻿using AbstractFactory.Contracts;
+using AbstractFactory.Models;
+using System;
+
+namespace AbstractFactory.Factories
+{
+    public class HondaFactory : IVehicleFactory
+    {
+        public Bike GetBike(string bike)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Scooter GetScooter(string scooter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
