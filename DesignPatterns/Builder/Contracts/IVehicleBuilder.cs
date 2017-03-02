f22b393c67c98d966503021ab71d147d5933e382
shopov91 <shopov91@gmail.com>
2017-03-02 19:58:46 +0200

    Added interface IVehicleBuilder.cs

---
+++ DesignPatterns/Builder/Contracts/IVehicleBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Builder.Models;
+
+namespace Builder.Contracts
+{
+    /// <summary>
+    /// The 'Builder' interface
+    /// </summary>
+    public interface IVehicleBuilder
+    {
+        void SetModel();
+
+        void SetEngine();
+
+        void SetTransmission();
+
+        void SetBody();
+
+        void SetAccessories();
+
+        Vehicle GetVehicle();
+    }
+}
